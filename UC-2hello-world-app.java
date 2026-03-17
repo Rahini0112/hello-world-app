1481465987e8bb4f15d69879c3574c7e8d2aa6a9
Rahini0112 <rt6071@srmist.edu.in>
2026-03-17 21:33:50 +0530

    Add files via upload

---
+++ UC-2hello-world-app.java
@@ -0,0 +1,9 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        // Directly access the first command-line argument
+        String name = args[0];
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
+    }
+}
